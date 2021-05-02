bf7e7c72dfed2f13f8700319efdc52e8616d139f
unknown <fewzi_krasny@outlook.fr>
2021-05-03 01:39:01 +0200

    [FRONT] [FOO-BAR-QUIX] Create css class for foo-bar-quix and foo-bar-quix-form component

--- foobarquix-ui/src/app/foo-bar-quix/foo-bar-quix-form/foo-bar-quix-form.component.ts
+++ foobarquix-ui/src/app/foo-bar-quix/foo-bar-quix-form/foo-bar-quix-form.component.ts
@@ -3,7 +3,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-foo-bar-quix-form',
-  templateUrl: './foo-bar-quix-form.component.html'
+  templateUrl: './foo-bar-quix-form.component.html',
+  styleUrls: ['./foo-bar-quix-form.component.scss']
 })
 export class FooBarQuixFormComponent implements OnInit {
 
