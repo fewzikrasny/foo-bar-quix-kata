31ccfc5db72aa19955dd86f5ba9b6ad8f19c30e3
unknown <fewzi_krasny@outlook.fr>
2021-05-02 02:13:10 +0200

    [BACK] [FOO-BAR-QUIX] Creating a method for build divisors and correspondences hashmap in FooBarQuixService

--- foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
+++ foobarquix-back/src/main/kotlin/com/kata/foobarquix/services/FooBarQuixService.kt
@@ -1,12 +1,25 @@
 package com.kata.foobarquix.services
 
+import com.kata.foobarquix.configuration.DivisorsAndCorrespondencesProperties
 import org.springframework.stereotype.Component
+import java.util.stream.Collectors
+import java.util.stream.IntStream
 
 @Component
-class FooBarQuixService {
+class FooBarQuixService(private val  divisorsAndCorrespondencesProperties: DivisorsAndCorrespondencesProperties) {
 
     fun convertNumber(inputNumber: Int): String {
         return inputNumber.toString();
     }
 
+    /**
+     * populate a hashmap from an divisors and correspondences properties
+     **/
+    private fun buildDivisorsAndCorrespondences(): HashMap<Int, String> {
+        val divisorsKeys: List<String> = divisorsAndCorrespondencesProperties.getDivisorsKeys().split(",")
+        val correspondencesValues: List<String> = divisorsAndCorrespondencesProperties.getCorrespondencesValues().split(",")
+        val divisorsAndCorrespondencesMap = IntStream.range(0, divisorsKeys.size).boxed()
+                .collect(Collectors.toMap({ i: Int? -> divisorsKeys[i!!].toInt() }) { i: Int? -> correspondencesValues[i!!] })
+        return HashMap(divisorsAndCorrespondencesMap)
+    }
 }
